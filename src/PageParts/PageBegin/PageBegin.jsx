6dea23944f75ba6c4e73e20f26002b420a388a47
Rottschaefer <rottschaefer54@gmail.com>
2024-07-15 01:17:32 -0300

    Initial Setup is now made

--- src/PageParts/PageBegin/PageBegin.jsx
+++ src/PageParts/PageBegin/PageBegin.jsx
@@ -1,15 +1,35 @@
 import {
+  StyledButton,
+  StyledForm,
+  StyledInput,
   StyledLogo,
   StyledPageBegin,
+  StyledPerfil,
   StyledSubTitle,
   StyledSubTitleSpecial,
   StyledUnderline,
 } from "./StyledPageBegin";
 import logo from "../../assets/MP - logo principal - branco.png";
+import perfil from "../../assets/perfil.png";
+import { useState } from "react";
 
 export const PageBegin = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    phone: "",
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
   return (
     <StyledPageBegin>
+      <StyledPerfil src={perfil} />
       <StyledLogo src={logo} />
       <StyledSubTitle>
         Conquiste{" "}
@@ -21,6 +41,42 @@ export const PageBegin = () => {
         no diagnóstico, procedimentos cirúrgicos e tratamento das alterações dos
         freios orais
       </StyledSubTitle>
+      <StyledForm>
+        <label>
+          <StyledInput
+            placeholder="Nome Completo"
+            type="text"
+            name="name"
+            value={formData.firstName}
+            onChange={handleInputChange}
+            required
+          />
+        </label>
+        <br />
+        <label>
+          <StyledInput
+            placeholder="Seu melhor Email"
+            type="email"
+            name="email"
+            value={formData.lastName}
+            onChange={handleInputChange}
+            required
+          />
+        </label>
+        <br />
+        <label>
+          <StyledInput
+            placeholder="WhatsApp com DDD"
+            type="text"
+            name="phone"
+            value={formData.email}
+            onChange={handleInputChange}
+            required
+          />
+        </label>
+        <br />
+        <StyledButton type="submit">Preencher Aplicação</StyledButton>
+      </StyledForm>
     </StyledPageBegin>
   );
 };
--- src/PageParts/PageBegin/StyledPageBegin.jsx
+++ src/PageParts/PageBegin/StyledPageBegin.jsx
@@ -1,19 +1,41 @@
 import styled, { keyframes } from "styled-components";
 
 export const StyledPageBegin = styled.div`
+  overflow-x: hidden;
+  position: relative;
   display: flex;
   flex-direction: column;
   justify-content: start;
   align-items: center;
   width: 100vw;
+  min-height: auto;
   height: 100vh;
+  padding-bottom: 3rem;
   background-color: #591527;
+  z-index: -1;
 `;
 
 export const StyledLogo = styled.img`
   width: 55vw;
+  margin-bottom: -15vw;
+  margin-top: -10vw;
+`;
+
+export const StyledPerfil = styled.img`
+  @media (min-width: 1000px) {
+    height: 100vw;
+    top: 500px;
+  }
+  position: absolute;
+
+  top: 400px;
+  left: -60px;
+  transform: translate(-50%, -50%);
+  height: 100vh;
   margin-bottom: -60px;
-  margin-top: -10px;
+  margin-top: -15px;
+  z-index: 0;
+  opacity: 0.4;
 `;
 
 export const StyledSubTitle = styled.h1`
@@ -21,17 +43,19 @@ export const StyledSubTitle = styled.h1`
   font-size: 2rem;
   text-align: center;
   color: #bfaaae;
+  z-index: 1;
+  width: 85vw;
 `;
 
 export const StyledSubTitleSpecial = styled.span`
   position: relative;
-
   font-family: "giarek";
   font-size: 2rem;
   text-align: center;
   /* text-decoration: underline; */
-  color: #bfaaae;
+  color: white;
   margin: 0;
+  z-index: 0;
 `;
 
 const underlineAnimation = keyframes`
@@ -45,9 +69,51 @@ const underlineAnimation = keyframes`
 
 export const StyledUnderline = styled.div`
   position: absolute;
-  top: 32px;
+  top: 93%;
   left: 0px;
   height: 2px;
   background-color: #bfaaae;
   animation: ${underlineAnimation} 1s ease-in-out 0.5s forwards;
+  z-index: -1;
+`;
+
+export const StyledForm = styled.form`
+  position: relative;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+  z-index: 0;
+`;
+
+export const StyledInput = styled.input`
+  font-family: "giarek";
+  font-weight: 800;
+  font-size: 1.2rem;
+  height: 2vh;
+  width: 76vw;
+  max-width: 400px;
+  border-radius: 10px;
+  border: none;
+  padding: 2vh;
+  border-color: #bfaaae;
+  border-width: 2px;
+  border-style: solid;
+`;
+
+export const StyledButton = styled.button`
+  background: linear-gradient(135deg, #f3b6b6, #fff);
+  border-radius: 10px;
+  border-color: #591527;
+  border-style: solid;
+  border-width: 5px;
+  font-family: "giarek";
+  font-size: 1.3rem;
+  font-weight: 800;
+  color: #591527;
+  height: 9vh;
+  width: 80vw;
+  max-width: 400px;
+  margin-top: 10px;
 `;
