8753188fdff1ca57fe5410d153407a86be203d7e
Rottschaefer <rottschaefer54@gmail.com>
2024-07-16 19:02:22 -0300

    Now the MVP is done. the program is able to post on the google sheets. After submission, the user is redirected to a determined forms

--- src/App.jsx
+++ src/App.jsx
@@ -9,25 +9,6 @@ import { ThirdPart } from "./PageParts/ThirdPart/ThirdPart";
 import { doc } from "./GoogleAuth";
 
 function App() {
-  async function testConnection() {
-    try {
-      await doc.loadInfo(); // Carregar informações básicas do documento
-
-      console.log(`Título da planilha: ${doc.title}`);
-      console.log(`Número de guias: ${doc.sheetCount}`);
-
-      // Exemplo de leitura de dados de uma planilha
-      const sheet = doc.sheetsByIndex[0];
-      const rows = await sheet.getRows();
-      console.log(rows);
-    } catch (error) {
-      console.error("Erro ao testar a conexão com o Google Sheets:", error);
-    }
-  }
-
-  useEffect(() => {
-    testConnection();
-  }, []);
   return (
     <>
       <GlobalStyle />
---
+++ src/Components/Loading/Loading.jsx
@@ -0,0 +1,9 @@
+import { StyledLoading, StyledLoadingConteiner } from "./StyledLoading";
+
+export const Loading = () => {
+  return (
+    <StyledLoadingConteiner>
+      <StyledLoading />
+    </StyledLoadingConteiner>
+  );
+};
